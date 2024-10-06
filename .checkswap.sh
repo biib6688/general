bb6a03fae16ab72ff73996c8d2ce9d08fdf3d590
biib6688 <hiepnt6688@gmail.com>
2024-10-06 08:08:48 +0700

    Update .checkswap.sh

--- .checkswap.sh
+++ .checkswap.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-# Check swap used > 2.5GB
+# Check swap used > 2GB
 USED_SWAP=$(free | grep Swap | awk '{print $3}')
-if [ "$USED_SWAP" -gt 2621440 ]; then
+if [ "$USED_SWAP" -gt 2097152 ]; then
   echo "Swap usage is greater than 2.5GB. Proceeding with service restart..."
   
   # Stop dill and hemi
@@ -16,5 +16,5 @@ if [ "$USED_SWAP" -gt 2621440 ]; then
   
   echo "Services dill and hemi have been restarted."
 else
-  echo "Swap usage is below 2.5GB. No action taken."
+  echo "Swap usage is below 2GB. No action taken."
 fi
